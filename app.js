e83de35e54f83f1fcb0927ceb36e534f7228ed80
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2024-06-13 12:23:32 +0530

    Full code added

--- app.js
+++ app.js
@@ -43,8 +43,24 @@ const btn = document.querySelector("#throttle");
                div.style.top = dets.clientY+'px';
                document.body.appendChild(div) //append div to the body
 
-               var div = document.createElement("img");
-               img.setAttribute("src", )
+               var img = document.createElement("img");
+               img.setAttribute("src","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS31NXLW_Xi50Szm_k9_JAl9YVyOhNhGOxBsA&s" );
+               div.appendChild(img);
+
+               //to make img visible from bottom
+               gsap.to(img, {
+                y:"0",
+                ease: Power1,
+                duration:0.5,
+               })
+
+               //to remove that appeared img
+               gsap.to(img, {
+                y:"100%",
+                ease: Power2,
+                delay: .8,
+               })
+
                //to remove that div
                setTimeout(function(){
                 div.remove();
